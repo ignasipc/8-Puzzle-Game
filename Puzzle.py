76f6566b02bf7074e286a47123e70e52d8a4ac77
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-11-03 10:48:11 +0100

    All images painted and started pygame painting

--- Puzzle.py
+++ Puzzle.py
@@ -1,9 +1,36 @@
+import pygame
+import sys
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+images_table = {
+    1: "1.png",
+    2: "2.png",
+    3: "3.png",
+    4: "4.png",
+    5: "5.png",
+    6: "6.png",
+    7: "7.png",
+    8: "8.png",
+}
+
 class Puzzle:
     # Class variables
+    window = pygame.display.set_mode((3*40 + 4*5, 3*40 + 4*5))  # 3*3 puzzle + 5 pixels between pieces and board margins
+    window.fill(pygame.Color(0,0,0))
+    pygame.display.set_caption("8-Puzzle Game")
 
+    pygame.init()
+
+    # Constructor
     def __init__(self, dimension, combination):
         self.dimension = dimension
         self.combination = combination
 
 
+
     def draw(self):
+        img = []
+        for i in range(self.dimension[0]*self.dimension[1]):
+            img[i]=pygame.image.load(images_table[i])
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 from Puzzle import Puzzle
-import pygame
-import sys
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-images_table = ()
 
 def main():
-    window = pygame.display.set_mode((120, 120))
-    window.fill(pygame.Color(WHITE))
-    pygame.display.set_caption("8-Puzzle Game")
-
-
-
-
     dimension = (3,3)   # 3x3 Puzzle
+
     combination = (3,7,2,5,4,1,6,8,0)
+    solution_combination = (1,2,3,4,5,6,7,8,0)
+
     starting_puzzle_state = Puzzle(dimension, combination)
-
-    Puzzle(dimension, )
-
-    pygame.quit()
-    sys.exit()
 
 
 if __name__ == "__main__":
