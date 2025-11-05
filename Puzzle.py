3fcc06a448478cfb3b74d5d8582fd9b8da278025
Ignasi Paredes Casasnovas <ignasipc22@gmail.com>
2025-11-05 14:25:23 +0100

    Drawing completed, moving animation in progress

--- Puzzle.py
+++ Puzzle.py
@@ -23,14 +23,108 @@ class Puzzle:
 
     pygame.init()
 
+
     # Constructor
-    def __init__(self, dimension, combination):
+    def __init__(self, dimension, combination, solution_combination):
         self.dimension = dimension
         self.combination = combination
+        self.solution = solution_combination
 
+
+        self.draw()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                #if event == movement -> draw movement
 
 
     def draw(self):
         img = []
         for i in range(self.dimension[0]*self.dimension[1]):
-            img[i]=pygame.image.load(images_table[i])
+            if self.combination[i] != 0:
+                img.append(pygame.image.load(images_table[self.combination[i]]))
+            else:
+                img.append(None)
+
+        displacement_row = 5
+        for row in range(self.dimension[0]):
+            displacement_col = 5
+            for column in range(self.dimension[1]):
+                try:
+                    self.window.blit(img[column+row*3], (column*40 + displacement_col, row*40 + displacement_row))
+                except Exception:
+                    continue
+                displacement_col += 5
+            displacement_row += 5
+
+        pygame.display.flip()
+
+
+    def draw_movement(self, movement):
+        empty_space_pos = self.search_empty_space()
+        animation_time = 2.0    # Time in seconds of the animation
+        img_moving_piece = None
+        moving_piece_pos = None
+
+        # We suppose the movement is valid
+        moving_number = None
+        match movement:
+            case "UP":
+                moving_number = self.combination[empty_space_pos-3]
+                moving_piece_pos = empty_space_pos-3
+            case "RIGHT":
+                moving_number = self.combination[empty_space_pos+1]
+                moving_piece_pos = empty_space_pos+1
+            case "DOWN":
+                moving_number = self.combination[empty_space_pos+3]
+                moving_piece_pos = empty_space_pos+3
+            case "LEFT":
+                moving_number = self.combination[empty_space_pos-1]
+                moving_piece_pos = empty_space_pos-1
+
+
+        img_moving_piece = pygame.image.load(images_table[moving_number])
+
+        initial_pos_x = moving_piece_pos % self.dimension[0] * 45 + 5
+        initial_pos_y = moving_piece_pos // self.dimension[1] * 45 + 5
+
+
+
+
+
+    3,7,2,5,4,0,6,8,1
+
+    3,7,2
+    5,4,0
+    6,8,1
+
+
+
+    def search_empty_space(self) -> int :
+        """
+        :return: Position of empty space in puzzle array
+        """
+        return self.combination.index(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from Puzzle import Puzzle
 def main():
     dimension = (3,3)   # 3x3 Puzzle
 
-    combination = (3,7,2,5,4,1,6,8,0)
+    combination = (3,7,2,5,4,0,6,8,1)
     solution_combination = (1,2,3,4,5,6,7,8,0)
 
-    starting_puzzle_state = Puzzle(dimension, combination)
+    starting_puzzle_state = Puzzle(dimension, combination, solution_combination)
 
 
 if __name__ == "__main__":
